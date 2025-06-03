3ef97f6dae9414399fd718d9931355800825d671
Moenen Erbuer <moenen.erbuer@ibm.com>
2025-06-03 19:30:36 +0300

    -

---
+++ main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
+
+from pydantic import BaseModel
+
+# Tool
+from viz_tools import render_molecule_svg_2d, render_molecule_svg_3d
+
+app = FastAPI(
+    title="Molecule Visualization API using RDKit",
+    version="1.0.0",
+    description="Returns 2D visualizations of molecules in SVG format using RDKit.",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class SmilesPayload(BaseModel):
+    smiles: str
+
+
+# Smiles as query parameter
+@app.get("/render-molecule-svg", summary="SVG image of a small molecule")
+def render_molecule_svg(
+    smiles: str = Query(..., description="SMILES string of the molecule"),
+    d3: bool = Query(False, description="Render in 3D if True, otherwise render in 2D"),
+):
+    """
+    Render an SVG image of a small molecule from a SMILES string provided as query parameter.
+
+    Example: http://localhost:8035/render-smol-svg?smiles=C1=CC=CC=C1
+    """
+    if d3:
+        svg_str = render_molecule_svg_3d(smiles)
+    else:
+        svg_str = render_molecule_svg_2d(smiles)
+
+    # Fail
+    if svg_str is None:
+        print("ERROR generating SVG for SMILES:", smiles)
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid SMILES string, unable to generate SVG: {smiles}",
+        )
+
+    print("Success generating SVG for SMILES:", smiles)
+    print("SVG:\n", svg_str)
+
+    # Success
+    return Response(
+        content=svg_str,
+        media_type="image/svg+xml",
+        headers={"Content-Disposition": f'inline; filename="{smiles}.svg"'},
+    )
---
+++ viz_tools/render_molecule_svg_3d.py
@@ -0,0 +1,77 @@
+import re
+import time
+from random import randint
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+from cinemol.api import Atom, Bond, Look, Style, draw_molecule
+
+
+def render_molecule_svg_3d(smiles: str) -> str:
+    """
+    Render 3D molecule from SMILES string to SVG format.
+
+    Args:
+        smiles (str): The SMILES string of the molecule
+
+    Returns:
+        str: SVG representation of the molecule
+    """
+    mol = Chem.MolFromSmiles(smiles)
+    # Generate conformer
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol, useRandomCoords=True, randomSeed=0xF00D)
+    AllChem.MMFFOptimizeMolecule(mol)
+    pos = mol.GetConformer().GetPositions()
+
+    # Parse atoms and bonds from molecule.
+    atoms, bonds = [], []
+
+    for atom in mol.GetAtoms():
+        if atom.GetSymbol() == "H":
+            continue
+        color = (220, 220, 200)
+        atoms.append(
+            Atom(atom.GetIdx(), atom.GetSymbol(), pos[atom.GetIdx()], color=color)
+        )
+
+    for bond in mol.GetBonds():
+        if (
+            bond.GetBeginAtom().GetSymbol() == "H"
+            or bond.GetEndAtom().GetSymbol() == "H"
+        ):
+            continue
+        start_index, end_index = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+        bonds.append(Bond(start_index, end_index, int(bond.GetBondTypeAsDouble())))
+
+    t0 = time.time()
+
+    # Draw molecule.
+    svg = draw_molecule(
+        atoms=atoms,
+        bonds=bonds,
+        style=Style.BALL_AND_STICK,
+        look=Look.CARTOON,
+        resolution=50,
+        # Not obvious: 6 = 360°
+        rotation_over_y_axis=randint(0, 600) / 100,
+        rotation_over_x_axis=randint(0, 600) / 100,
+        rotation_over_z_axis=randint(0, 600) / 100,
+        # view_box=(0, -0, 2000, 2000),  # (x, y, width, height)
+        scale=50,
+    )
+
+    # Success
+    svg_str = svg.to_svg()
+
+    # Add width & height
+    regex_pattern = r'(<svg[^>]*?viewBox="[^"]*?")'
+    replacement_string = r'\1 width="600" height="600">'
+    svg_str = re.sub(regex_pattern, replacement_string, svg_str)
+
+    # Report
+    svg_size = len(svg.to_svg()) / 1000
+    print(f"Runtime: {1000 * (time.time() - t0)} ms")
+    print(f"File size: {svg_size} kb")
+
+    return svg_str
