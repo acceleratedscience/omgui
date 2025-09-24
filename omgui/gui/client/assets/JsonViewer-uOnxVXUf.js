9f56986286788e6bf990f573c3f7036716be3344
Moenen Erbuer <moenen.erbuer@ibm.com>
2025-09-24 19:06:49 +0300

    remove gitignore files

--- omgui/gui/client/assets/JsonViewer-uOnxVXUf.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./JsonViewer.vue_vue_type_script_setup_true_lang-DtDR6jmD.js";import"./main-BdmB6v6t.js";import"./BreadCrumbs-CpmEjbk3.js";import"./TextBox-CKJiqptr.js";export{o as default};
